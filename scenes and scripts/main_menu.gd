45050d085dd3b8976b7919eb6b49d2f75e6cd843
Sámson Bálint <balintsamson@gmail.com>
2025-03-21 02:34:02 +0100

    1.15
    -small changes to the menu - now the selected button is easier to see
    -repositioned the ammo label
    -small changes in the to-do list

--- scenes and scripts/main_menu.gd
+++ scenes and scripts/main_menu.gd
@@ -1,11 +1,25 @@
 extends Control
 
 @onready var start_button = $VBoxContainer/StartButton
+@onready var quit_button = $VBoxContainer/QuitButton
+
 var main_scene : String = "res://scenes and scripts/main.tscn"
 
 
 func _ready() -> void:
 	start_button.grab_focus()
+
+
+func _process(_delta: float) -> void:
+	if start_button.has_focus():
+		start_button.text = ">Start<"
+	else:
+		start_button.text = "Start"
+	
+	if quit_button.has_focus():
+		quit_button.text = ">Quit<"
+	else:
+		quit_button.text = "Quit"
 
 
 func _on_start_button_pressed() -> void:
